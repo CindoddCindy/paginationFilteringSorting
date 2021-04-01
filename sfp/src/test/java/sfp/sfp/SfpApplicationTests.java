8cc845f29bf08b10203cba67eec9c15983dde258
CindoddCindy <cindodcindy@gmail.com>
2021-04-01 22:05:25 +0700

    tambah package

---
+++ sfp/src/test/java/sfp/sfp/SfpApplicationTests.java
@@ -0,0 +1,13 @@
+package sfp.sfp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
